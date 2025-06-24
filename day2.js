0d75732fcf9dfb637e63f4f940cb72f76f9a736c
tamnna-sharma55 <tamnnasharmash55@gmail.com>
2025-06-24 15:20:28 +0530

    add files

---
+++ day2.js
@@ -0,0 +1,37 @@
+// let heading=document.getElementById("heading")
+// heading.textContent = "javascript"
+// heading.innerHTML= "<h1> javascript </h1>"
+
+// let heading = document.getElementsByClassName("heading")[1]
+// heading.textContent = "javascript"
+
+// let heading = document.getElementsByTagName("h2")[0]
+// heading.textContent = "javascript"
+
+// let image = document.getElementById("image")
+// image.setAttribute("src","https://img.freepik.com/free-psd/spectacular-flight-vivid-green-bird_191095-78393.jpg?uid=R121056201&ga=GA1.1.1865533930.1738668265&semt=ais_items_boosted&w=740")
+
+// image.removeAttribute("image")
+
+// let srcValue = image.getAttribute("src")
+// image.removeAttribute(srcValue)
+
+// image.removeAttribute("src")
+
+// let para = document.getElementById("para")
+// para.style.color = "red"
+// para.style.fontSize = "30px"
+
+
+// para.classList.add("head")
+// para.classList.remove("head")
+
+// let button= document.getElementById("button")
+// function toggleclass(){
+//     button.classList.toggle("head")
+// }
+
+let heading = document.createElement("h1")
+heading.textContent = "javascript"
+// head.appendChild("heading")
+document.body.appendChild(heading)
